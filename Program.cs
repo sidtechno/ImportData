c0524c6dac8e57bff2d9c9d4fcc2144b543eb6f6
Eric Dugal <eric.dugal@hotmail.com>
2023-03-04 14:41:41 -0500

    fix

--- Program.cs
+++ Program.cs
@@ -19,14 +19,14 @@ namespace ImportData
         static void Main(string[] args)
         {
             Console.WriteLine("Début de l'importation");
-            var garageId = 5587;
+            var garageId = 5581;
             Console.WriteLine("Obtenir les clients du fichier csv");
             var clients = GetClients();
             Console.WriteLine("Obtenir les vehicules du fichier csv");
             var vehicles = GetVehicles();
-            Console.WriteLine("Obtenir les historique du fichier csv");
-            var historiques = GetHistoriques();
-            var fix1 = GetHistoriquesFix1();
+            //Console.WriteLine("Obtenir les historique du fichier csv");
+            //var historiques = GetHistoriques();
+            //var fix1 = GetHistoriquesFix1();
 
             //Console.WriteLine("Obtenir les produits du fichier csv");
             //var products = GetProducts();
@@ -35,7 +35,8 @@ namespace ImportData
             Console.WriteLine("*** Importation des clients et véhicule ***");
             Console.WriteLine("********************************************");
 
-            ImportVehiculeClient(garageId, 2492, vehicles, clients, historiques, fix1);
+            ImportVehiculeClient(garageId, vehicles, clients);
+            //ImportVehiculeClientWithMaintenance(garageId, 2492, vehicles, clients, historiques, fix1);
 
             Console.WriteLine("********************************************");
             Console.WriteLine("*** Importation des historiques             ***");
@@ -127,7 +128,161 @@ namespace ImportData
             return result;
         }
 
-        public static void ImportVehiculeClient(int garageId, int baseMaintenancePlanId, IEnumerable<VehicleModel> vehicles, IEnumerable<ClientModel> clients, IEnumerable<HistoriqueModel> historiques, IEnumerable<HistoriqueFixModel> fix1)
+        public static void ImportVehiculeClient(int garageId, IEnumerable<VehicleModel> vehicles, IEnumerable<ClientModel> clients)
+        {
+            try
+            {
+                var sqlOwner = @"INSERT INTO [dbo].[VehicleOwner]
+                                ([Company] 
+                                ,[Name]
+                                ,[Address]
+                                ,[Phone] 
+                                ,[Email]
+                                ,[GarageId])
+                                OUTPUT INSERTED.Id
+                                VALUES
+                                (@OwnerCompany
+                                , @OwnerName
+                                , @OwnerAddress
+                                , @OwnerPhone
+                                , @OwnerEmail
+                                , @GarageId)";
+
+
+                var sql = @"INSERT INTO [dbo].[Vehicle2]
+		               ([Vincode]
+                      ,[Description]
+                      ,[Year]
+                      ,[Make]
+                      ,[Model]
+                      ,[Engine]
+                      ,[Transmission]
+                      ,[Propulsion]
+                      ,[BrakeSystem]
+                      ,[Steering]
+                      ,[Color]
+                      ,[UnitNo]
+                      ,[LicencePlate]
+                      ,[Seating]
+                      ,[Odometer]
+                      ,[SelectedUnit]
+                      ,[EntryDate]
+                      ,[MonthlyMileage]
+                      ,[OilTypeId]
+                      ,[MaintenancePlanId]
+                      ,[VehicleOwnerId]
+                      ,[VehicleDriverId]
+                      ,[GarageId])
+                    OUTPUT INSERTED.Id
+	                VALUES(
+		                 @Vincode
+                        ,@Description
+                        ,@Year
+                        ,@Make
+                        ,@Model
+                        ,@Engine
+                        ,@Transmission
+                        ,@Propulsion
+                        ,@BrakeSystem
+                        ,@Steering
+                        ,@Color
+                        ,@UnitNo
+                        ,@LicencePlate
+                        ,@Seating
+                        ,@Odometer
+                        ,@SelectedUnit
+                        ,@EntryDate
+                        ,@MonthlyMileage
+                        ,@OilTypeId
+                        ,@MaintenancePlanId
+                        ,@VehicleOwnerId
+                        ,@VehicleDriverId
+                        ,@GarageId)";
+
+
+
+                using (var connection = new SqlConnection("Data Source=tcp:s8ch2o0eft.database.windows.net,1433;Initial Catalog=OCHPlanner2;User ID=mecanimax@s8ch2o0eft;Password=Mecan1m@x;Trusted_Connection=False;"))
+                {
+                    connection.Open();
+
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        foreach (var client in clients)
+                        {
+                            //insert owner
+                            var OwnerInserted = connection.QuerySingle<int>(sqlOwner,
+                                new
+                                {
+                                    OwnerCompany = client.Compagnie,
+                                    OwnerName = client.Nom,
+                                    OwnerAddress = client.Adresse,
+                                    OwnerPhone = client.Phone,
+                                    OwnerEmail = client.Email,
+                                    GarageId = garageId
+
+                                },
+                                commandType: CommandType.Text,
+                                transaction: transaction);
+
+                            Console.WriteLine($"Traitement du client {client.Nom}");
+
+                            //Get vehicules for client
+                            var clientVehicule = vehicles.Where(p => p.VehicleOwnerId.Equals(client.Id, StringComparison.InvariantCultureIgnoreCase)).ToList();
+                            if (clientVehicule.Any())
+                            {
+                                var vehlist = new List<VehicleDatabaseModel>();
+                                var historyList = new List<WorkOrderDatabaseModel>();
+
+                                clientVehicule.ForEach(p =>
+                                {
+                                    Console.WriteLine($"Traitement du vehicule {p.Description}");
+
+                                    vehlist.Add(new VehicleDatabaseModel()
+                                    {
+                                        VinCode = p.VinCode.Length > 20 ? p.VinCode.Substring(0, 20) : p.VinCode,
+                                        Description = p.Description.Length > 200 ? p.Description.Substring(0, 200) : p.Description,
+                                        Year = p.Year,
+                                        Make = p.Make.Length > 75 ? p.Make.Substring(0, 75) : p.Make,
+                                        Model = p.Model.Length > 75 ? p.Model.Substring(0, 75) : p.Model,
+                                        Engine = p.Engine.Length > 75 ? p.Engine.Substring(0, 75) : p.Engine,
+                                        Propulsion = p.Propulsion.Length > 75 ? p.Propulsion.Substring(0, 75) : p.Propulsion,
+                                        Transmission = p.Transmission.Length > 75 ? p.Transmission.Substring(0, 75) : p.Transmission,
+                                        BrakeSystem = p.BrakeSystem.Length > 75 ? p.BrakeSystem.Substring(0, 75) : p.BrakeSystem,
+                                        Steering = p.Steering.Length > 75 ? p.Steering.Substring(0, 75) : p.Steering,
+                                        Color = p.Color.Length > 75 ? p.Color.Substring(0, 75) : p.Color,
+                                        UnitNo = p.UnitNo.Length > 25 ? p.UnitNo.Substring(0, 25) : p.UnitNo,
+                                        LicencePlate = p.Licence.Length > 12 ? p.Licence.Substring(0, 12) : p.Licence,
+                                        Seating = p.Seating,
+                                        Odometer = p.Odometer,
+                                        SelectedUnit = p.SelectedUnit.Length > 2 ? p.SelectedUnit.Substring(0, 2) : p.SelectedUnit,
+                                        EntryDate = new DateTime(p.Year, 6, 1).ToString("yyyy-MM-dd"),
+                                        MonthlyMileage = p.MonthlyMileage,
+                                        OilTypeId = 0,
+                                        MaintenancePlanId = 0,
+                                        VehicleOwnerId = OwnerInserted,
+                                        VehicleDriverId = 0,
+                                        GarageId = garageId
+                                    });
+
+                                });
+
+                                var affectedRows = connection.Execute(sql, vehlist, transaction: transaction);
+                            }
+
+                        }
+
+                        transaction.Commit();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur lors du traitement");
+                throw ex;
+            }
+        }
+
+        public static void ImportVehiculeClientWithMaintenance(int garageId, int baseMaintenancePlanId, IEnumerable<VehicleModel> vehicles, IEnumerable<ClientModel> clients, IEnumerable<HistoriqueModel> historiques, IEnumerable<HistoriqueFixModel> fix1)
         {
             try
             {
@@ -267,7 +422,7 @@ namespace ImportData
                         var plans = connection.Query<MaintenanceTypeModel>(sqlPlanType, new { GarageId = garageId }, commandType: CommandType.Text, transaction: transaction);
                         var basePlanDetail = connection.Query<MaintenancePlanDetailModel>(sqlMaintenanceDetailPlanBase, new { baseMPID = baseMaintenancePlanId }, commandType: CommandType.Text, transaction: transaction);
 
-                        foreach (var client in clients.Skip(10))
+                        foreach (var client in clients)
                         {
                             //insert owner
                             var OwnerInserted = connection.QuerySingle<int>(sqlOwner,
